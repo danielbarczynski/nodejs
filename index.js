4dd4596d00fb4027d5d695b19185bead1d3f3ce7
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-05-14 20:50:29 +0200

    module.exports, require

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('hello world!');
+
+const Person = require('./person');
+const person = new Person('Daniel', 23);
+console.log(person);
---
+++ person.js
@@ -0,0 +1,13 @@
+// const person = {
+//     name: 'Daniel',
+//     age: 23
+// }
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+module.exports = Person;
