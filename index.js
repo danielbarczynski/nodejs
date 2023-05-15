1cf6b04ff67c90a44aba7f380309e99dfddba83d
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-05-15 20:53:57 +0200

    endpoints to pages, json

--- index.js
+++ index.js
@@ -11,12 +11,38 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const { throws } = require('assert');
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/')
-    res.write('hey');
-  res.end();
+    fs.readFile(path.join(__dirname, 'public', 'home.html'), (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'text/html'})
+      res.end(content);
+    });
+  
+  else if (req.url === '/about')
+  fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    if (err) throw err;
+    res.writeHead(200, { 'Content-Type': 'text/html'})
+    res.end(content);
+  });
+
+  else if (req.url === '/api/users') {
+    const users = [
+      {
+        name: 'Daniel',
+        age: 23 
+      },
+      {
+        name: 'Mela',
+        age: 21
+      }
+    ]
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    res.end(JSON.stringify(users));
+  }
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
